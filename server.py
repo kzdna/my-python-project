e2313d2c1dd3e178862df06d7b469b29905f739d
Salsa Arindina <salsadina2005@gmail.com>
2026-05-05 23:27:37 +0700

    Update server.py

--- server.py
+++ server.py
@@ -1,15 +1,15 @@
-# 1. Panggil fungsi detector
-response = emotion_detector(text_to_analyze)
+@app.route("/emotionDetector")
+def sent_analyzer():
+    text_to_analyze = request.args.get('textToAnalyze')
+    response = emotion_detector(text_to_analyze)
+    dominant_emotion = response['dominant_emotion']
 
-# 2. Ambil dominant_emotion-nya dulu
-dominant_emotion = response['dominant_emotion']
+    if dominant_emotion is None:
+        return "Invalid text! Please try again!."
 
-# 3. CEK DULU DI SINI (Error Handling)
-if dominant_emotion is None:
-    return "Invalid text! Please try again!."
-
-# 4. Kalau tidak None, baru jalankan return yang panjang (angka-angka)
-return (
-    f"For the given statement, the system response is 'anger': {response['anger']}... "
-    f"The dominant emotion is {dominant_emotion}."
-)
+    return (
+        f"For the given statement, the system response is 'anger': {response['anger']}, "
+        f"'disgust': {response['disgust']}, 'fear': {response['fear']}, "
+        f"'joy': {response['joy']} and 'sadness': {response['sadness']}. "
+        f"The dominant emotion is {dominant_emotion}."
+    )
